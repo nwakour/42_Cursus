013a432310ad8a5668fa4b839f948bdf862fe19f
Nizar Wakour <nwakour@e1r4p6.1337.ma>
2020-10-17 16:50:34 +0100

    clear

--- cub3D/src/texture.c
+++ cub3D/src/texture.c
@@ -11,34 +11,6 @@ int	take_texture(t_mlx *mlx, t_tex *tex)
 	return (1);
 }
 
-/*int put_texture(t_all *all, int i, int j, int wall_bot)
-{
-	int y;
-	int x;
-
-	x = i;
-	y = 0;
-	while (j < wall_bot)
-	{
-		if (x > TILE_SIZE)
-			x = 0;
-		if (y > TILE_SIZE)
-			y = 0;
-		if (all->ray[i].ray_cord == 0)
-			all->mlx.img_data[WINDOW_WIDTH * j + i] = all->tex[TEX_NO].tex[x][y];
-		else if (all->ray[i].ray_cord == 1)
-			all->mlx.img_data[WINDOW_WIDTH * j + i] = 0x450000;
-		else if (all->ray[i].ray_cord == 2)
-			all->mlx.img_data[WINDOW_WIDTH * j + i] = 0x004500;
-		else if (all->ray[i].ray_cord == 3)
-			all->mlx.img_data[WINDOW_WIDTH * j + i] = 0x000045;
-		j++;
-		y++;
-	}
-	return j;
-}
-*/
-
 void		init_textute(t_all *all)
 {
  	take_texture(&all->mlx, &all->tex[TEX_NO]);
