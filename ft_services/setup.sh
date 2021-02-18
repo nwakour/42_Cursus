27ec375cdf453a951934f17a2beaa52395e393f1
Nizar Wakour <nwakour@e1r3p8.1337.ma>
2021-02-18 18:49:13 +0100

    hh

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -2,6 +2,6 @@ minikube delete
 minikube start
 minikube addons enable metallb
 eval $(minikube docker-env)
-docker build -t nginx_service ./srcs/test/srcs/.
-kubectl apply -f metallb.yaml
-kubectl apply -f nginx.yaml
+docker build -t nginx_service ./srcs/nginx/.
+kubectl apply -f yaml/metallb.yaml
+kubectl apply -f yaml/nginx.yaml
