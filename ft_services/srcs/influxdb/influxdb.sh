7dc606b88b6308c825580603d3d0fead07d53017
Nizar Wakour <nwakour@e1r6p2.1337.ma>
2021-03-08 19:35:42 +0100

    almost

--- ft_services/srcs/influxdb/influxdb.sh
+++ ft_services/srcs/influxdb/influxdb.sh
@@ -1,15 +1,10 @@
 #!bin/bash
-mkdir -p /etc/telegraf
-cp telegraf.conf /etc/telegraf
-#cp influxdb.conf /etc/
+mkdir -p etc/telegraf
+cp telegraf.conf etc/telegraf
+cp influxdb.conf etc/
 
-# mkdir -p /run/openrc
-# touch /run/openrc/softlevel
-# openrc reboot
-# rc-service telegraf start
-# rc-service influxdb start
-mkdir -p /var/run/
-touch  /var/run/utmp
+mkdir -p var/run/
+touch  var/run/utmp
 nohup influxd run &
 influx -execute "CREATE DATABASE telegraf"
 influx -execute "CREATE USER admin WITH PASSWORD 'admin' WITH ALL PRIVILEGES"
--- ft_services/srcs/mysql/mysql.sh
+++ ft_services/srcs/mysql/mysql.sh
@@ -1,14 +1,14 @@
 #!bin/bash
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-mkdir -p /run/openrc
-touch /run/openrc/softlevel
+mysql_install_db --user=mysql --datadir=var/lib/mysql
+mkdir -p run/openrc
+touch run/openrc/softlevel
 openrc reboot
 
 rc-update add mariadb default
 
 rc-service mariadb start
 
-chown -R mysql: /var/lib/mysql
+chown -R mysql: var/lib/mysql
 mysql -u root -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
 mysql -u root -e "CREATE DATABASE wp_db; use wp_db; source wordpress.sql;"
 mysql -u root -e "CREATE DATABASE phpmyadmin; use phpmyadmin; source phpmyadmin.sql;"
